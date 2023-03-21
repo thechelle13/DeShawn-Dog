83c8cac7f50457d10f19b469b85f57a51ca64de6
Michelle Totherow <tym4me13@gmail.com>
2023-03-21 11:23:36 -0500

    first commit

---
+++ scripts/Assignments.js
@@ -0,0 +1,39 @@
+import { getPets, getWalkers } from "./database.js"
+
+// Get copy of state for use in this module
+const pets = getPets()
+const walkers = getWalkers()
+
+
+// Function whose responsibility is to find the walker assigned to a pet
+const findWalker = (getPets, getWalkers) => {
+    let petWalker = null
+
+    for (const walker of getWalkers) {
+        if (walker.id === getPets.walkerId) {
+            petWalker = walker
+        }
+    }
+
+    return petWalker
+}
+
+export const Assignments = () => {
+    let assignmentHTML = ""
+    assignmentHTML = "<ul>"
+
+    for (const currentPet of pets) {
+        const currentPetWalker = findWalker(currentPet, walkers)
+        assignmentHTML = `
+            <li>
+                ${currentPet.name} is being walked by
+                ${currentPetWalker.name} in ${currentPetWalker.city}
+            </li>
+        `
+    }
+
+    assignmentHTML += "</ul>"
+
+    return assignmentHTML
+}
+
